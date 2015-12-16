715f1d4f518a5c9e8240a2d8f38f6ee9c46d76ea
Joe Amenta <airbreather@linux.com>
2015-12-15 19:40:48 -0500

    Calling this 1.0.1.

--- build.bat
+++ build.bat
@@ -10,20 +10,20 @@ if exist mods\journey*.jar (
 )
 
 :rest
-7za a -r cacophony_inc-1.0.0.zip config hats mods scripts
+7za a -r cacophony_inc-1.0.1.zip config hats mods scripts
 popd
-move data\cacophony_inc-1.0.0.zip .
+move data\cacophony_inc-1.0.1.zip .
 
-copy cacophony_inc-1.0.0.zip cacophony_inc_nojourneymap-1.0.0.zip
-7za d cacophony_inc_nojourneymap-1.0.0.zip mods\journeymap*.jar
+copy cacophony_inc-1.0.1.zip cacophony_inc_nojourneymap-1.0.1.zip
+7za d cacophony_inc_nojourneymap-1.0.1.zip mods\journeymap*.jar
 
 pushd server-skeleton
-7za x -r ..\cacophony_inc_nojourneymap-1.0.0.zip
+7za x -r ..\cacophony_inc_nojourneymap-1.0.1.zip
 del mods\neiaddons-*.jar
 del mods\neiintegration-*.jar
 
-7za a -r cacophony_inc_server-1.0.0.zip *
+7za a -r cacophony_inc_server-1.0.1.zip *
 del /s /q config hats mods scripts
 rd /s /q config hats mods scripts
 popd
-move server-skeleton\cacophony_inc_server-1.0.0.zip .
+move server-skeleton\cacophony_inc_server-1.0.1.zip .
