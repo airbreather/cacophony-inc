1a86d844441395c067b28016bf0a97719ea0a2c0
Joe Amenta <airbreather@linux.com>
2016-01-07 21:01:53 -0500

    Moving everything to a "minecraft" subfolder.

--- build.bat
+++ build.bat
@@ -3,26 +3,26 @@ set VERSION=1.0.2
 
 cd %~dp0
 pushd data
-if not exist mods\journey*.jar (
+if not exist minecraft\mods\journey*.jar (
 	popd
 	echo You forgot journey map
 	goto :eof
 )
 
-7za a -r cacophony_inc-%VERSION%.zip config hats mods scripts
+7za a -r cacophony_inc-%VERSION%.zip minecraft
 popd
 move data\cacophony_inc-%VERSION%.zip .
 
 copy cacophony_inc-%VERSION%.zip cacophony_inc_nojourneymap-%VERSION%.zip
-7za d cacophony_inc_nojourneymap-%VERSION%.zip mods\journeymap*.jar
+7za d cacophony_inc_nojourneymap-%VERSION%.zip minecraft\mods\journeymap*.jar
 
 pushd server-skeleton
 7za x -r ..\cacophony_inc_nojourneymap-%VERSION%.zip
-del mods\neiaddons-*.jar
-del mods\neiintegration-*.jar
+del minecraft\mods\neiaddons-*.jar
+del minecraft\mods\neiintegration-*.jar
 
 7za a -r cacophony_inc_server-%VERSION%.zip *
-del /s /q config hats mods scripts
-rd /s /q config hats mods scripts
+del /s /q minecraft
+rd /s /q minecraft
 popd
 move server-skeleton\cacophony_inc_server-%VERSION%.zip .
